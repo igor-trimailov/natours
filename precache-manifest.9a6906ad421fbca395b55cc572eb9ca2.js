68bb364935783ce781b06bd36b8f5a0adad585d0
Igoris Trimailovas <igoris.trimailovas@sgdigital.com>
2020-07-25 14:36:20 +0100

    Updates

---
+++ precache-manifest.9a6906ad421fbca395b55cc572eb9ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3ad7adec0a1e222a5d606f5c6fbbfa",
+    "url": "/natours/index.html"
+  },
+  {
+    "revision": "4523b6e0d5dba4264403",
+    "url": "/natours/static/css/main.c04d33db.chunk.css"
+  },
+  {
+    "revision": "38a009c2fa9634b84a17",
+    "url": "/natours/static/js/2.3661a48e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/natours/static/js/2.3661a48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4523b6e0d5dba4264403",
+    "url": "/natours/static/js/main.5e85b0dd.chunk.js"
+  },
+  {
+    "revision": "5f712d35cf2fa20ceecf",
+    "url": "/natours/static/js/runtime-main.790254ec.js"
+  }
+]);
